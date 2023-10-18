f4df333018af98bd103ddb253ce0c2545c014876
burger <fsm15@naver.com>
2023-10-19 00:50:56 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class='app'>
+   <Ground />
+ </div>
+</template>
+
+<script setup>
+import Ground from './components/Ground.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Ground.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class='ground'>I am Ground 자기소개하기</div>
+</template>
+
+<style scoped>
+
+</style>
